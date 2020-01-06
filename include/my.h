66f19a0c3e5415812eb3f78d3b695635cc1d55cc
Quentin TREHEUX <qtreheux@localhost.localdomain>
2020-01-06 09:27:32 +0100

    [ADD] makefile && src with main && include with my.h

---
+++ include/my.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2019
+** my.h
+** File description:
+** matchstick
+*/
+
+#ifndef MY_H_
+#define MY_H_
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#endif
